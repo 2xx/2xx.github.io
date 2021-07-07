5fb908ca2a3fb3f9210d0c190b9438f5c16335cc
h2o <h2o_zs@163.com>
2021-07-07 18:58:57 +0800

    init

---
+++ src/templates/Post.vue
@@ -0,0 +1,45 @@
+<template>
+  <Layout>
+    <div>
+      <h1>{{ $page.post.title }}</h1>
+      <div>{{ $page.post.content }}</div>
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+query ($id: ID!) {
+  post (id: $id) {
+    id
+    title
+    content
+  }
+}
+</page-query>
+
+<script>
+import MarkdownIt from 'markdown-it'
+const md = new MarkdownIt();
+// var result = md.render('# hell');
+
+export default {
+  name: 'PostPage',
+  metaInfo () {
+    return {
+      title: this.$page.post.title
+    }
+  },
+  methods:{
+      mdToHtml(markdown) {
+          return md.render(markdown);
+      }
+  } 
+  // metaInfo: {
+  //   title: ''
+  // }
+}
+</script>
+
+<style>
+
+</style>
